ff2fdf95d295290e9400913c3db2da9d828dca31
Zach Kelling <z@zeekay.io>
2026-07-11 15:51:42 -0700

    Debrand exporters: clickhouse{traces,logs}exporter -> datastore{traces,logs}exporter
    Rename the two exporters Hanzo owns to drop the "clickhouse" brand:
    - exporter/clickhousetracesexporter -> exporter/datastoretracesexporter
      (package, component id clickhousetraces -> datastoretraces)
    - exporter/clickhouselogsexporter -> exporter/datastorelogsexporter
      (package, component id clickhouselogsexporter -> datastorelogsexporter)
    - symbols: WithClickHouseClient -> WithDatastoreClient, newClickhouseClient ->
      newDatastoreClient, pushToClickhouse -> pushToDatastore,
      clickhouseTracesExporter -> datastoreTracesExporter; files
      clickhouse_exporter*.go -> datastore_exporter*.go.
    
    Left intact (external, not ours): github.com/ClickHouse/clickhouse-go/v2 wire
    driver (clickhouse.Open/Conn/ParseDSN) and github.com/srikanthccv/
    ClickHouse-go-mock test helper (cmock.NewClickHouseNative /
    NewClickHouseWithQueryMatcher). Sibling clickhousemetricswrite exporter and
    clickhousesystemtablesreceiver are out of scope.
    
    go build ./components/ and go vet of both packages pass.

---
+++ exporter/datastorelogsexporter/config.go
@@ -0,0 +1,82 @@
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datastorelogsexporter
+
+import (
+	"errors"
+	"time"
+
+	"github.com/hanzoai/otel-collector/utils"
+	"go.opentelemetry.io/collector/config/configoptional"
+	"go.opentelemetry.io/collector/config/configretry"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+	"go.uber.org/multierr"
+)
+
+const (
+	defaultPromotedPathsSyncInterval     = 5 * time.Minute
+	defaultMaxAllowedDataAgeDays     int = 15
+)
+
+type AttributesLimits struct {
+	FetchKeysInterval time.Duration `mapstructure:"fetch_keys_interval"`
+	MaxDistinctValues int           `mapstructure:"max_distinct_values"`
+}
+
+// Config defines configuration for ClickHouse exporter.
+type Config struct {
+	exporterhelper.TimeoutConfig `mapstructure:",squash"`
+	BackOffConfig                configretry.BackOffConfig                                `mapstructure:"retry_on_failure"`
+	QueueBatchConfig             configoptional.Optional[exporterhelper.QueueBatchConfig] `mapstructure:"sending_queue"`
+
+	// DSN is the ClickHouse server Data Source Name.
+	// For tcp protocol reference: [ClickHouse/clickhouse-go#dsn](https://github.com/ClickHouse/clickhouse-go#dsn).
+	DSN                 string `mapstructure:"dsn"`
+	UseNewSchema        bool   `mapstructure:"use_new_schema"`
+	LogLevelConcurrency *int   `mapstructure:"log_level_concurrency"`
+
+	AttributesLimits          AttributesLimits `mapstructure:"attributes_limits"`
+	PromotedPathsSyncInterval *time.Duration   `mapstructure:"promoted_paths_sync_interval"`
+	BodyJSONEnabled           bool             `mapstructure:"body_json_enabled"`
+	BodyJSONOldBodyEnabled    bool             `mapstructure:"body_json_old_body_enabled"`
+	// MaxAllowedDataAgeDays drops logs older than now minus this many days.
+	MaxAllowedDataAgeDays *int `mapstructure:"max_allowed_data_age_days"`
+}
+
+var (
+	errConfigNoDSN = errors.New("dsn must be specified")
+)
+
+// Validate validates the clickhouse server configuration.
+func (cfg *Config) Validate() (err error) {
+	if cfg.DSN == "" {
+		err = multierr.Append(err, errConfigNoDSN)
+	}
+	if cfg.PromotedPathsSyncInterval == nil {
+		cfg.PromotedPathsSyncInterval = utils.ToPointer(defaultPromotedPathsSyncInterval)
+	}
+
+	if cfg.LogLevelConcurrency == nil {
+		cfg.LogLevelConcurrency = utils.ToPointer(utils.Concurrency())
+	}
+	if *cfg.LogLevelConcurrency < 1 {
+		err = multierr.Append(err, errors.New("concurrency must be greater than 0"))
+	}
+	if cfg.MaxAllowedDataAgeDays == nil {
+		cfg.MaxAllowedDataAgeDays = utils.ToPointer(defaultMaxAllowedDataAgeDays)
+	}
+
+	return err
+}
